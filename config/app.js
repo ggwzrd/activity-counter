3b96b4f3bbd8ead6fdae41afb004d503a31d5607
giuliogallerini <giulio.gallerini.work@hotmail.com>
2016-08-16 17:31:18 +0200

    add activities
    - effect of the run and gym icon.
    - removed bug on the animation.
    - added css for fonts, inputs, containers, p and span.
    - added the “add run” activity.

--- config/app.js
+++ config/app.js
@@ -4,6 +4,62 @@ var main = function (){
 		$('.upline').toggleClass('rotate')
 		$('.downline').toggleClass('rev-rotate')
 		$('.navbar').toggleClass('nav-open')
+		$('.content-container').toggleClass('nav-open')
+	});
+
+	$('.running').mouseenter(function(){
+		$(this).addClass('running-hover')
+	});
+
+	$('.running').mouseout(function(){
+		if(!$('.selected')[0]) {
+			$(this).removeClass('running-hover')
+		}
+	});
+
+	$('.running').click(function(){
+		$(this).animate({left: '200px'})
+		$(this).addClass('running-hover')
+		$(this).addClass('selected')
+		$('.gym').animate({
+			opacity :0
+		}, 500);
+		$('.activities').animate({
+			opacity :50
+		}, 2000);
+
+	});
+
+	$('.gym').mouseenter(function(){
+		$(this).addClass('gym-hover')
+	});
+
+	$('.gym').mouseout(function(){
+		if(!$('.selected')[0]) {
+			$(this).removeClass('gym-hover')
+		}
+	});
+
+	$('.gym').click(function(){
+		$(this).animate({right: '200px'})
+		$(this).addClass('gym-hover')
+		$(this).addClass('selected')
+		$('.running').animate({
+			opacity :0
+		}, 500)
+
+	});
+
+	$('.close').click(function(){
+		$('.activities').animate({
+			opacity :0
+		}, 500)
+		$('.running').removeClass('selected')
+		$('.running').removeClass('running-hover')
+		$('.running').animate({left: '0px'})
+		$('.gym').animate({
+			opacity: 100
+		})
 	});
 };
 
